952e5894f103650c5307f0881eb6343a05daeb7c
k1105 <kanapon1105@yahoo.co.jp>
2025-03-18 17:13:43 +0900

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,54 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-
+import {YouTubeEmbed} from "@next/third-parties/google";
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <main className={styles.main}>
+      <div className={styles.firstView}>気配の花</div>
+      <div className={styles.section}>
+        <p className={styles.statement}>
+          ちゃんと元気でいるか、気になる。
+          <br />
+          <span className={styles.segment}>けれど、毎日連絡をとるのは</span>
+          <span className={styles.segment}>なかなか難しい。</span>
+          <br />
+          離れて暮らす、大切な人とのあいだに
+          <br />
+          <span className={styles.segment}>お互いの気配を気軽に感じる</span>
+          <span className={styles.segment}>きっかけができました。</span>
+          <br />
+          <span className={styles.segment}>
+            「おはよう」「いってきます」「ただいま」
+          </span>
+          <br />
+          <span className={styles.segment}>3つの声に反応して、</span>
+          <span className={styles.segment}>香りを届けます。</span>
+          <br /> <span className={styles.segment}>ゆるやかに でもたしかに</span>
+          <br />
+          <span className={styles.segment}>私たちは つながっている。</span>
+        </p>
+      </div>
+      <div className={styles.section}>
+        <h2 className={styles.headline}>紹介動画</h2>
+        <div className={styles.movieContainer}>
+          <div className={styles.movieWrapper}>
+            <YouTubeEmbed videoid="k2i73lmL3CM" />
+          </div>
+          <div className={styles.textWrapper}>
+            <p>
+              懐かしい香りがして、
+              <br />
+              ふと誰かのことを思い出した経験はありませんか？
+              <br />
+              「気配の花」はそんな香りの力を使って
+              <br />
+              大切な人にあなたの気配を届けます。
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className={styles.section}>
+        <h2 className={styles.headline}>使い方</h2>
+      </div>
+    </main>
   );
 }
